d4b83505339753826c744f59623bb991e0ab4454
Not-James-Bond <chaitanyasingh92@yahoo.com>
2022-09-09 17:53:08 +0530

    Started server and hosted p5.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("Socket is Running")
+const express = require('express');
+const app = express();
+
+const server = app.listen(8001);
+app.use(express.static('public'));
+
+
+console.log("Socket is Running");
