c280bb777c0866118044af3ebe0b3d6ccce369a3
Not-James-Bond <chaitanyasingh92@yahoo.com>
2022-09-09 19:25:47 +0530

    Sockets Connection Working

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,29 @@
+let socket;
+const address = process.env.SERVER_ADDRESS;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 550);
+  background(51);
+
+  socket = io.connect(`http://localhost:8001`);
+  socket.on('mouseCordinates', data => {
+    noStroke();
+    fill(255, 0, 100);
+    ellipse(data.X, data.Y, 10, 10);
+  });
 }
 
-function draw() {
-  background(50);
+function mouseDragged() {
+  console.log(`Sending Mouse Corrdinates ${mouseX}, ${mouseY}`);
+  const data = {
+    X: mouseX,
+    Y: mouseY,
+  };
+  socket.emit('mouseCordinates', data);
+
+  noStroke();
+  fill(255);
   ellipse(mouseX, mouseY, 10, 10);
 }
+
+function draw() {}
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const socket = require('socket.io');
 const app = express();
 
-const server = app.listen(8001);
+const port = process.env.PORT;
+const server = app.listen(port);
+console.log('Socket is Running');
 app.use(express.static('public'));
 
+const io = socket(server);
 
-console.log("Socket is Running");
+io.on('connection', socket => {
+  console.log('Socket Server New Connection: ' + socket.id);
+
+  socket.on('mouseCordinates', data => {
+    console.log(`Receiving Mouse Corrdinates: ${data.X},${data.Y}`);
+    socket.broadcast.emit('mouseCordinates', data); //It emits to other client exluding the current client  
+    // io.sockets.emit('mouseCordinates', data); //It emits to all the clients including the current client
+  });
+});
